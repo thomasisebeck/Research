62ea4935c79a15bb74c33de8d16c3720e643b91b
Thomas Isebeck <tisebeck@proton.me>
2026-07-27 11:41:19 +0200

    finished zig dyn poly

--- zig/src/dyn_poly.zig
+++ zig/src/dyn_poly.zig
@@ -44,23 +44,37 @@ pub fn makeSoundHelper(comptime myAnimal: anytype) void {
     myAnimal.sound();
 }
 
-pub fn main() void {
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
     var cat = Cat{};
     var dog = Dog{};
     var mouse = Mouse{};
 
-    // init the zoo with all componenst
-    const zoo = [_]Component{
-        asCat(&cat),
-        asDog(&dog),
-        asBird(&mouse),
-    };
+    const ZOO_SIZE = 100_000;
+    var zoo: [ZOO_SIZE]Component = undefined;
 
-    // TODO:  start timer
+    // Populate the array with each animal
+    var i: usize = 0;
+    while (i < ZOO_SIZE) : (i += 3) {
+        if (i + 0 < ZOO_SIZE) zoo[i + 0] = asCat(&cat);
+        if (i + 1 < ZOO_SIZE) zoo[i + 1] = asDog(&dog);
+        if (i + 2 < ZOO_SIZE) zoo[i + 2] = asBird(&mouse);
+    }
+
+    var start_time = std.Io.Clock.now(.awake, io);
+
+    // 3. Run the dynamic polymorphism benchmark loop
     for (zoo) |animal| {
         const result = animal.sound();
-        std.debug.print("An animal says: {s}\n", .{result});
-    }
-    // TODO: end timer
 
+        // makes sure that its not removed
+        std.mem.doNotOptimizeAway(result);
+    }
+
+    const end_time = std.Io.Clock.now(.awake, io);
+
+    const duration = start_time.durationTo(end_time);
+
+    std.debug.print("Processed in: {} ns\n", .{duration.toNanoseconds()});
 }
