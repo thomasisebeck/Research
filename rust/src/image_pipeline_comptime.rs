5af798b556b31bfe9743e4b2070b4c0933a50431
Thomas Isebeck <tisebeck@proton.me>
2026-07-27 11:07:19 +0200

    finished image pipeline

--- rust/src/image_pipeline_comptime.rs
+++ rust/src/image_pipeline_comptime.rs
@@ -1,4 +1,5 @@
 mod utils;
+use std::time::Instant;
 
 enum Mode {
     HIGH,
@@ -57,28 +58,23 @@ impl PipelineConfig for LowQualityConfig {
     const APPLY_SATURATION: bool = true;
 }
 
-struct Colour {
-    pub r: f32,
-    pub g: f32,
-    pub b: f32,
-}
-
 // <> is the lifetime param
-pub struct Neighbours<'a> {
-    pub top_left: &'a Colour,
-    pub middle_left: &'a Colour,
-    pub bottom_left: &'a Colour,
-    pub bottom_middle: &'a Colour,
-    pub bottom_right: &'a Colour,
-    pub middle_right: &'a Colour,
-    pub top_right: &'a Colour,
-    pub top_middle: &'a Colour,
+#[derive(Clone, Copy)] // to copy out of the node
+pub struct Neighbours {
+    pub top_left: utils::Colour,
+    pub middle_left: utils::Colour,
+    pub bottom_left: utils::Colour,
+    pub bottom_middle: utils::Colour,
+    pub bottom_right: utils::Colour,
+    pub middle_right: utils::Colour,
+    pub top_right: utils::Colour,
+    pub top_middle: utils::Colour,
 }
 
 // if statement
 fn quantise<CFG: PipelineConfig>(colour: f32) -> f32 {
     let res;
-    if matches!(CFG::QUANTISE_MODE, Mode::LOW) {
+    if matches!(CFG::QUANTISE_MODE, Mode::HIGH) {
         res = f32::round(colour * 255.0) / 255.0;
     } else if matches!(CFG::QUANTISE_MODE, Mode::MED) {
         res = f32::round(colour * 16.0) / 16.0;
@@ -90,7 +86,7 @@ fn quantise<CFG: PipelineConfig>(colour: f32) -> f32 {
 }
 
 // switch statement
-fn blur<CFG: PipelineConfig>(item: &mut Colour, n: Neighbours) {
+fn blur<CFG: PipelineConfig>(item: &mut utils::Colour, n: Neighbours) {
     match CFG::BLUR_MODE {
         Mode::LOW => {
             item.r = (n.middle_left.r + item.r + n.middle_right.r) / 3.0;
@@ -144,7 +140,7 @@ fn blur<CFG: PipelineConfig>(item: &mut Colour, n: Neighbours) {
 }
 
 // switch expression
-fn saturation<CFG: PipelineConfig>(item: &mut Colour) {
+fn saturation<CFG: PipelineConfig>(item: &mut utils::Colour) {
     let LUMA: f32 = (0.299 * item.r) + (0.587 * item.g) + (0.144 * item.b);
 
     let delta: f32 = match CFG::COLOUR_MODE {
@@ -154,58 +150,109 @@ fn saturation<CFG: PipelineConfig>(item: &mut Colour) {
     };
 
     item.r = (LUMA + (delta * (item.r - LUMA))).clamp(0.0, 1.0);
-    item.g = LUMA + (delta * (item.g - LUMA)).clamp(0.0, 1.0);
-    item.b = LUMA + (delta * (item.b - LUMA)).clamp(0.0, 1.0);
+    item.g = (LUMA + (delta * (item.g - LUMA))).clamp(0.0, 1.0);
+    item.b = (LUMA + (delta * (item.b - LUMA))).clamp(0.0, 1.0);
 }
 
 // constexpr void process(utils::Colour (&mat)[utils::SIZE][utils::SIZE]) {
-fn process<CFG: PipelineConfig>(mat: &mut [[Colour; utils::SIZE]; utils::SIZE]) {
-    for row_num in 1..=(utils::SIZE - 1) {
-        for col_num in 1..=(utils::SIZE - 1) {
-            let item: &mut Colour = &mut mat[row_num][col_num];
-
+fn process<CFG: PipelineConfig>(mat: &mut [[utils::Colour; utils::SIZE]; utils::SIZE]) {
+    // not ..= (we want exclusive loop)
+    for row_num in 1..(utils::SIZE - 1) {
+        for col_num in 1..(utils::SIZE - 1) {
             if CFG::APPLY_BLUR {
                 // these are non-mutable refs
                 // no need to fight with the borrow checker
+
+                // must be immutable
                 let n = Neighbours {
-                    top_left: &mat[row_num - 1][col_num - 1],
-                    middle_left: &mat[row_num][col_num - 1],
-                    bottom_left: &mat[row_num + 1][col_num - 1],
-                    bottom_middle: &mat[row_num + 1][col_num],
-                    bottom_right: &mat[row_num + 1][col_num + 1],
-                    middle_right: &mat[row_num][col_num + 1],
-                    top_right: &mat[row_num - 1][col_num + 1],
-                    top_middle: &mat[row_num - 1][col_num],
+                    top_left: mat[row_num - 1][col_num - 1],
+                    middle_left: mat[row_num][col_num - 1],
+                    bottom_left: mat[row_num + 1][col_num - 1],
+                    bottom_middle: mat[row_num + 1][col_num],
+                    bottom_right: mat[row_num + 1][col_num + 1],
+                    middle_right: mat[row_num][col_num + 1],
+                    top_right: mat[row_num - 1][col_num + 1],
+                    top_middle: mat[row_num - 1][col_num],
                 };
+
+                // need to grab it here
+                // because we want to copy out the mat refs
+                // before we take the middle one as mutable
+                let item: &mut utils::Colour = &mut mat[row_num][col_num];
+
+                // must be mutable
+                blur::<CFG>(item, n);
+            }
+
+            if CFG::APPLY_QUANTIZATION {
+                // grab a mutable ref
+                let item: &mut utils::Colour = &mut mat[row_num][col_num];
+
+                item.r = quantise::<CFG>(item.r);
+                item.g = quantise::<CFG>(item.g);
+                item.b = quantise::<CFG>(item.b);
+            }
+
+            if CFG::APPLY_SATURATION {
+                let item: &mut utils::Colour = &mut mat[row_num][col_num];
+
+                // pass the entire item in
+                saturation::<CFG>(item);
             }
         }
     }
 }
 
+fn main() {
+    // have to init the array in rust
+    let init_pixel = utils::Colour {
+        r: 0.0,
+        g: 0.0,
+        b: 0.0,
+    };
+
+    // pass the init pixel in here
+    // type is derived
+    let mut my_image = [[init_pixel; utils::SIZE]; utils::SIZE];
+
+    // pass the image in as mutable
+    utils::read_image_from_file("input_image.txt", &mut my_image);
+
+    let start_time = Instant::now();
+
+    // configs are declared above as traits...
+    process::<HighQualityConfig>(&mut my_image);
+
+    let duration = start_time.elapsed();
+
+    let nanoseconds = duration.as_nanos();
+
+    println!("Processed in: {} ns", nanoseconds);
+}
+
 /*
+*
 
 
-constexpr void process(utils::Colour (&mat)[utils::SIZE][utils::SIZE]) {
-  for (int row_num = 1; row_num < utils::SIZE - 1; row_num++) {
 
-    for (int col_num = 1; col_num < utils::SIZE - 1; col_num++) {
-      // ref to the item
-      utils::Colour &item = mat[row_num][col_num];
+ constexpr auto config =
+     utils::PipelineConfig{.colour_mode = utils::Mode::LOW,
+                           .blur_mode = utils::Mode::LOW,
+                           .apply_blur = true,
+                           .sharpen_mode = utils::Mode::LOW,
+                           .quantise_mode = utils::Mode::LOW,
+                           .apply_quantisation = true,
+                           .saturation_mode = utils::Mode::LOW,
+                           .apply_saturation = true};
 
-      if constexpr (cfg.apply_blur) {
-        const utils::Neighbours n = {
-            .topLeft = mat[row_num - 1][col_num - 1],
-            .middleLeft = mat[row_num][col_num - 1],
-            .bottomLeft = mat[row_num + 1][col_num - 1],
-            .bottomMiddle = mat[row_num + 1][col_num],
-            .bottomRight = mat[row_num + 1][col_num + 1],
-            .middleRight = mat[row_num][col_num + 1],
-            .topRight = mat[row_num - 1][col_num + 1],
-            .topMiddle = mat[row_num - 1][col_num]
+ const auto start_time = std::chrono::steady_clock::now();
 
-        };
+ process<config>(my_image);
 
-        blur<cfg>(item, n);
-      };
+ const auto end_time = std::chrono::steady_clock::now();
+ const auto duration = end_time - start_time;
+
+ const auto nanoseconds =
+     std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
+ std::print("Processed in: {} ns\n", nanoseconds);
 */
-fn main() {}
--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,6 +1,14 @@
+use std::fs;
 use std::fs::read_to_string;
 
-pub const SIZE: usize = 500;
+pub const SIZE: usize = 50;
+
+#[derive(Clone, Copy)]
+pub struct Colour {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
 
 pub fn read_array_from_file<const STEPS: usize>(path: &str) -> [f64; STEPS] {
     let mut my_arr: [f64; STEPS] = [0.0; STEPS];
@@ -25,4 +33,40 @@ pub fn print_array<const Size: usize>(arr: &[i32; Size]) {
     }
 
     print!("]\n");
+}
+
+pub fn read_image_from_file(path: &str, mat: &mut [[Colour; SIZE]; SIZE]) {
+    let content = fs::read_to_string(path).expect("Cannot open file");
+
+    // split by both spaces and newlines
+    // will split the rgb values as well
+    let mut tokens = content.split_whitespace();
+    let mut counter = 0;
+
+    for row in 0..SIZE {
+        for col in 0..SIZE {
+            // 3. Extract r, g, and b
+            let r: f32 = tokens
+                .next()
+                .and_then(|t| t.parse().ok())
+                .expect("File corrupted");
+
+            let g: f32 = tokens
+                .next()
+                .and_then(|t| t.parse().ok())
+                .expect("File corrupted");
+
+            let b: f32 = tokens
+                .next()
+                .and_then(|t| t.parse().ok())
+                .expect("File corrupted");
+
+            // Put them in the matrix
+            mat[row][col].r = r;
+            mat[row][col].g = g;
+            mat[row][col].b = b;
+
+            counter += 1;
+        }
+    }
 }
